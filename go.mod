2959e48ceb1894390e934ec0d6ded562511357a0
wellfrogjp <wellfrogjp@yahoo.co.jp>
2023-04-12 16:01:30 +0900

    test2

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/wellfrog/gotest1
+
+go 1.20
+
+require (
+
+	github.com/envoyproxy/protoc-gen-validate v0.10.1 // indirect
+	github.com/iancoleman/strcase v0.2.0 // indirect
+	github.com/lyft/protoc-gen-star/v2 v2.0.1 // indirect
+	github.com/spf13/afero v1.3.3 // indirect
+	golang.org/x/lint v0.0.0-20210508222113-6edffad5e616 // indirect
+	golang.org/x/mod v0.9.0 // indirect
+	golang.org/x/net v0.8.0 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	golang.org/x/text v0.8.0 // indirect
+	golang.org/x/tools v0.7.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+
+	. "github.com/wellfrog/gotest1/generated/example"
+)
+
+func main() {
+	p := new(Person)
+
+	err := p.Validate() // err: Id must be greater than 999
+	fmt.Println(err)
+	p.Id = 1000
+
+	err = p.Validate() // err: Email must be a valid email address
+	p.Email = "example@bufbuild.com"
+
+	err = p.Validate() // err: Name must match pattern '^[^\d\s]+( [^\d\s]+)*$'
+	p.Name = "Protocol Buffer"
+
+	err = p.Validate() // err: Home is required
+	p.Home = &Person_Location{Lat: 37.7, Lng: 999}
+
+	err = p.Validate() // err: Home.Lng must be within [-180, 180]
+	p.Home.Lng = -122.4
+
+	err = p.Validate() // err: nil
+}
